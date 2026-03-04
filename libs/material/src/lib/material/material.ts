90806fe1a6049c3af6161896340c07ebe2066d65
robert-brightline <robert.brightline@gmail.com>
2026-03-04 13:34:47 -0600

    initial

---
+++ libs/material/src/lib/material/material.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-material',
+  imports: [],
+  templateUrl: './material.html',
+  styleUrl: './material.css',
+})
+export class Material {}
