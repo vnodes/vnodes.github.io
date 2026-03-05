339884b9a620c116549a13257b916024ff901a0b
robert-brightline <robert.brightline@gmail.com>
2026-03-04 20:35:57 -0600

    fix: fixed initial page loading

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,6 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /// <reference types="@angular/localize" />
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+
+async function main() {
+
+    await bootstrapApplication(App, appConfig)
+
+
+    {
+        const browserLang = navigator.language || (navigator as any).userLanguage;
+        const supportedLocales = ['es', 'fr', 'ru', 'tr', 'za'];
+        const defaultLocale = 'en';
+        const targetLocale = supportedLocales.find(locale =>
+            browserLang.toLowerCase().startsWith(locale)
+        ) || defaultLocale;
+
+        console.log(`Location Path: '${window.location.pathname}'`)
+
+        if (window.location.pathname === '/') {
+            window.location.href = window.location.pathname + targetLocale + '/';
+        }
+    }
+}
+
+
+main()
