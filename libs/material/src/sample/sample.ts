3f7b8234c2d7347e8f7b4f101960fc16c06c348e
robert-brightline <robert.brightline@gmail.com>
2026-04-21 15:27:13 -0500

    feat: updated the generator plugin

---
+++ libs/material/src/sample/sample.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vn-sample',
+  standalone: true,
+  imports: [],
+  template: `<p>sample</p>`,
+  styles: ``,
+})
+export class SampleComponent { }
