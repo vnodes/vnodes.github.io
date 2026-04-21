25f4ef177fe703308bd1758812e017a70d1ec9b1
robert-brightline <robert.brightline@gmail.com>
2026-04-20 19:52:23 -0500

    feat: configured the ng-package build

--- libs/material/src/public-api.ts
+++ libs/material/src/public-api.ts
@@ -1,10 +1,10 @@
 // @index(['./**/public-api.ts'], f => `export * from '${f.path}'`)
-// export * from './flex/public-api'
-// export * from './form-sample/public-api'
-// export * from './form/public-api'
-// export * from './input-number/public-api'
-// export * from './input-text/public-api'
-// export * from './input/public-api'
-// export * from './number-filter/public-api'
-export const vnodesMaterial = 'vnodesMaterial'
+export * from './flex/public-api'
+export * from './form-sample/public-api'
+export * from './form/public-api'
+export * from './input-number/public-api'
+export * from './input-text/public-api'
+export * from './input/public-api'
+export * from './number-filter/public-api'
+export * from './utils/public-api'
 
