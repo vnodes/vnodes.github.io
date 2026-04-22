0bbecc6719907e0ec61ac08cff90121338b3ddcc
robert-brightline <robert.brightline@gmail.com>
2026-04-22 16:27:44 -0500

    fix: fixed the publish configuration

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -1,17 +1,19 @@
 import {
   ApplicationConfig,
-  provideBrowserGlobalErrorListeners, isDevMode,
+  isDevMode,
+  provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(appRoutes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
 };
