0de284562e1533c08cc7bcdc5d7591bcd3369c6f
robert-brightline <robert.brightline@gmail.com>
2026-04-29 20:06:48 -0500

    docs: configured the github page deployment

---
+++ libs/material/src/checkbox/checkbox.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { expect } from 'storybook/test';
+import { CheckboxComponent } from './checkbox';
+
+const meta: Meta<CheckboxComponent> = {
+  component: CheckboxComponent,
+  title: 'CheckboxComponent',
+};
+export default meta;
+
+type Story = StoryObj<CheckboxComponent>;
+
+export const Primary: Story = {
+  args: {
+    label: "Checkbox"
+  },
+};
+
+export const Heading: Story = {
+  ...Primary,
+  play: async ({ canvas }) => {
+    await expect(canvas.getByText(/Checkbox/gi)).toBeTruthy();
+  },
+};
---
+++ libs/material/src/checkbox/checkbox.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { BaseInput } from '@vnodes/material/input';
+
+@Component({
+  selector: 'vn-checkbox',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatInputModule
+  ],
+  template: `
+  <mat-checkbox
+       matInput
+       [formControl]="formControl()"
+       [disabled]="disabled()"
+       (input)="handleInput($event)"
+       (blur)="handleBlur()"
+       [minlength]="minlength()"
+       [maxlength]="maxlength()"
+       [required]="required()"
+  >
+  {{label()}}
+  </mat-checkbox>
+  `
+})
+export class CheckboxComponent extends BaseInput {
+
+
+  protected override convertToValue(value: string) {
+    return value
+  }
+
+}
