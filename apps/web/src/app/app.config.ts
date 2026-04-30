69016edbbb4a0ccf2402bd82af769e6b35937f45
robert-brightline <robert.brightline@gmail.com>
2026-04-30 13:19:02 -0500

    feat: added hash location to the web app

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -3,14 +3,14 @@ import {
   isDevMode,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes, withHashLocation()),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000'
